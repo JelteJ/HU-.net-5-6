ab9cf736cd978706859845f82a10d1ccbb64fad6
Jelte Jaarsma <jelte.jaarsma+github@gmail.com>
2015-05-26 09:54:08 +0200

    Basis aangemaakt
    simpele loginservice met ingeprogrammeerd username en password

---
+++ WinkelService/WinkelService/LoginService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WinkelService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LoginService" in both code and config file together.
+    public class LoginService : ILoginService
+    {
+        public string login(string username, string password)
+        {
+            if (username =="jaap" && password == "password123") {
+
+                return "Success";
+            }
+            return "Dikke fucking stront";
+        }
+    }
+}

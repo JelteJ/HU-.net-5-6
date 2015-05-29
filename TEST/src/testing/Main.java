8965696cc151a7729175b8b2f206a09f47a97e3d
Jelte Jaarsma <jelte.jaarsma+github@gmail.com>
2015-05-29 14:41:08 +0200

    Vermakkelijkt van aanroepen services

--- TEST/src/testing/Main.java
+++ TEST/src/testing/Main.java
@@ -9,6 +9,8 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 import org.tempuri.AddProduct;
+import org.tempuri.ILoginService;
+import org.tempuri.ILogisticsService;
 import org.tempuri.IProductService;
 import org.tempuri.Login;
 import org.tempuri.LoginService;
@@ -18,7 +20,46 @@ import org.tempuri.ProductService;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean b = getLoginServiceInterface().login("abc", "cba");
+
+		System.out.println(b);
+	}
+
+	public static ILoginService getLoginServiceInterface() {
+		URL wsdlURL = null;
+		try {
+			wsdlURL = new URL(
+					"http://localhost:8733/Design_Time_Addresses/WinkelService/LoginService/?wsdl ");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		QName serviceName = new QName("http://tempuri.org/", "LoginService");
+		Service service = Service.create(wsdlURL, serviceName);
+		// Dit is de SEI
+		ILoginService i = service.getPort(ILoginService.class);
+
+		return i;
+	}
+	
+	public static ILogisticsService getLogisticsServiceInterface() {
+		URL wsdlURL = null;
+		try {
+			wsdlURL = new URL(
+					"http://localhost:8733/Design_Time_Addresses/WinkelService/LogisticsService/?wsdl ");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		QName serviceName = new QName("http://tempuri.org/", "LogisticsService");
+		Service service = Service.create(wsdlURL, serviceName);
+		// Dit is de SEI
+		ILogisticsService i = service.getPort(ILogisticsService.class);
+
+		return i;
+	}
+	
+	public static IProductService getProductServiceInterface() {
 		URL wsdlURL = null;
 		try {
 			wsdlURL = new URL(
@@ -27,18 +68,11 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		QName serviceName = new QName("http://tempuri.org/",
-				"ProductService");
+		QName serviceName = new QName("http://tempuri.org/", "ProductService");
 		Service service = Service.create(wsdlURL, serviceName);
 		// Dit is de SEI
-		IProductService wsInterface = service.getPort(IProductService.class);
-		
-		AddProduct product = new AddProduct();
-		
-		boolean b = wsInterface.addProduct("testje", 100, 69);
-		
-		
-		System.out.println(b);
-	}
+		IProductService i = service.getPort(IProductService.class);
 
+		return i;
+	}
 }
